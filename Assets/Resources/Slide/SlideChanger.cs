cbf1f7120ed39b2a48531ac4df2313ea157e2ebe
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2018-03-05 22:53:18 +0900

    スクリーンとスライド画像の取得スクリプトの作成

---
+++ Assets/Resources/Slide/SlideChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SlideChanger : MonoBehaviour {
+
+	public GameObject targetScreen;
+	private List<string> slideList = new List<string>();
+	private Material currentSlide;
+
+	// Use this for initialization
+	void Start () {
+		GetTextureList (slideList, "Assets/Resources/Slide/");
+		//currentSlide.SetTexture ("_BumpMap", slideList [0]);
+		foreach (string path in slideList) 
+		{
+			Debug.Log (path);	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GetTextureList(List<string> slideList, string filepath)
+	{
+
+		string[] files = Directory.GetFiles(filepath, "*.jpg" );
+		foreach (var file in files)
+		{
+			slideList.Add(file);
+		}
+			
+	}
+}
